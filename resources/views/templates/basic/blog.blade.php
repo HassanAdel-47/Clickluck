d7330924f632ec34c711d6bf59f16d03423bb2f5
Hassan-47\HASSAN <Hassan@gmail.com>
2024-03-03 23:51:41 +0300

    Machine

--- resources/views/templates/basic/blog.blade.php
+++ resources/views/templates/basic/blog.blade.php
@@ -1,8 +1,10 @@
 @extends($activeTemplate . 'layouts.frontend')
 @section('content')
 <!-- blog section start -->
-<section class="pt-100 pb-50">
-    <div class="machine" style="background-image:  url('{{ asset($activeTemplateTrue . 'images/Machine.jpg')}}') ">
+<section>
+    <!-- <div class="machine"> -->
+    <div class="machine" style="background-image:  url('{{ asset($activeTemplateTrue . 'images/Machine1.jpg')}}') ">
+        <!-- <img src="{{ asset($activeTemplateTrue . 'images/Machine1.jpg')}}" alt=""> -->
         <div class="red">
             <div class="machine__counter">
                 <div id="odometer" class="odometer ">
@@ -30,12 +32,12 @@
                     0
                 </div>
             </div>
-            <div class="machineInfo">
-                <p>
-                    1st Place Winning Ticket:&nbsp; <span> - - - - - -</span>&nbsp;
-                    2nd Place Winning Ticket: <span>14m:60s</span> 3rd Place Winning Ticket: <span>29m:60s</span>
-                </p>
-            </div>
+        </div>
+        <div class="machineInfo">
+            <p>
+                1st Place Winning Ticket:&nbsp; <span> - - - - - -</span>&nbsp;
+                2nd Place Winning Ticket: <span>14m:60s</span> 3rd Place Winning Ticket: <span>29m:60s</span>
+            </p>
         </div>
 
     </div>
--- storage/framework/views/877d09b0f5ef0e5785b5ff8cb488d32bab707a67.php
+++ storage/framework/views/877d09b0f5ef0e5785b5ff8cb488d32bab707a67.php
@@ -1,7 +1,9 @@
 <?php $__env->startSection('content'); ?>
 <!-- blog section start -->
-<section class="pt-100 pb-50">
-    <div class="machine" style="background-image:  url('<?php echo e(asset($activeTemplateTrue . 'images/Machine.jpg')); ?>') ">
+<section>
+    <!-- <div class="machine"> -->
+    <div class="machine" style="background-image:  url('<?php echo e(asset($activeTemplateTrue . 'images/Machine1.jpg')); ?>') ">
+        <!-- <img src="<?php echo e(asset($activeTemplateTrue . 'images/Machine1.jpg')); ?>" alt=""> -->
         <div class="red">
             <div class="machine__counter">
                 <div id="odometer" class="odometer ">
@@ -29,12 +31,12 @@
                     0
                 </div>
             </div>
-            <div class="machineInfo">
-                <p>
-                    1st Place Winning Ticket:&nbsp; <span> - - - - - -</span>&nbsp;
-                    2nd Place Winning Ticket: <span>14m:60s</span> 3rd Place Winning Ticket: <span>29m:60s</span>
-                </p>
-            </div>
+        </div>
+        <div class="machineInfo">
+            <p>
+                1st Place Winning Ticket:&nbsp; <span> - - - - - -</span>&nbsp;
+                2nd Place Winning Ticket: <span>14m:60s</span> 3rd Place Winning Ticket: <span>29m:60s</span>
+            </p>
         </div>
 
     </div>
