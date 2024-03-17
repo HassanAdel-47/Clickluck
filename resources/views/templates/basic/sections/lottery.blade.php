795b5a931abd310660276681319c8c0da3cb7a36
ReemyHasan <reemhasan2000831@gmail.com>
2024-03-17 23:29:01 +0300

    fix route problem

--- resources/views/templates/basic/sections/lottery.blade.php
+++ resources/views/templates/basic/sections/lottery.blade.php
@@ -47,7 +47,7 @@
                             </p>
                         </div>
                         <a class="btn p-2 btn--base--message wow fadeInUp mt-2" data-wow-duration="0.5s"
-                            data-wow-delay="0.7s" href="{{ route('lottery.details', $phase->lottery->id) }}">
+                            data-wow-delay="0.7s" href="{{ route('lottery.details', $phase->id) }}">
                             Buy Ticket ${{ number_format($phase->lottery->price, 2) }}</a>
                         <div class="label__running d-flex justify-content-around align-items-between">
                             <p>Running</p>
